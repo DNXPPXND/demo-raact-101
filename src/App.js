ca7c8cbe6df016c7547a4ba875698757b597a057
DNXPPXND <jamwsmax8@gmail.com>
2023-03-12 16:04:58 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import logo from './assets/daisy.jpg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="App">
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DAISY  
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.pinterest.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/sign-up' element={<Signup />}></Route>
+        <Route path='/sign-in' element={<Signin />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+      </Routes>
+    </>
   );
 }
 
